9caca0f4b8f25c84d717a6a7b649e7d2de0fd368
Charhmwang <charwanghm@gmail.com>
2023-02-24 01:49:59 +0000

    Added Test5 and 6 for Tile and TileBag - Test5 passed, Test6 failed

--- src/pij/main/JUnitTest.java
+++ src/pij/main/JUnitTest.java
@@ -30,19 +30,48 @@ public class JUnitTest {
 
     @Test
     void testWord_in_WordList1() throws IOException {
-        System.out.println("Test 3: find words in Dictionary - class Dictionary, WordList");
+        System.out.println("Test 3: find words in class WordList");
         WordList wl = new WordList();
         boolean expected = true; // existing
         Assertions.assertEquals(expected, wl.validateWord("academic"));
     }
 
     @Test
-    void testWord_in_Dictionary2() throws IOException {
-        System.out.println("Test 4: find words in Dictionary - class Dictionary, WordList");
+    void testWord_in_WordList2() throws IOException {
+        System.out.println("Test 4: find words in class WordList");
         WordList wl = new WordList();
         boolean expected = false; // not existing
         Assertions.assertEquals(expected, wl.validateWord("zza"));
     }
+
+    @Test
+    void test_Tile_toString() {
+        System.out.println("Test 5: test Tile class and method toString");
+        Tile tile = new Tile('Q', 10);
+        String expected = "[Q10]";
+        String actual = tile.toString();
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    void test_TileBag() {
+        System.out.println("Test 6: test TileBag class and method isEmpty");
+        TileBag tb = new TileBag();
+        boolean expected = false;
+        boolean actual = tb.tilesInBag.isEmpty();
+        Assertions.assertEquals(expected, actual);
+    }
+
+//    @Test
+//    void test_TileBag_takeOutTile() {
+//        System.out.println("Test 7: test TileBag class and method takeOutTile");
+//        TileBag tb = new TileBag();
+//        Tile tile = new Tile('Q', 10);
+//        Tile getTile = tb.takeOutTile();
+//        Class<? extends Tile> actual = getTile.getClass();
+//        Class<? extends Tile> expected = tile.getClass();
+//        Assertions.assertEquals(expected, actual);
+//    }
 
 
 }
