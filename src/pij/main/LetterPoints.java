d6ffc4c549ef3814a3adeb69786fe5af867fa197
Charhmwang <charwanghm@gmail.com>
2023-02-24 01:20:17 +0000

    Created LetterPoints class for indexing letters points

---
+++ src/pij/main/LetterPoints.java
@@ -0,0 +1,38 @@
+package pij.main;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class LetterPoints {
+    public static final Map<Character, Integer> letterMap = new HashMap<>();
+
+    public LetterPoints() {
+        letterMap.put('A', 1);
+        letterMap.put('B', 3);
+        letterMap.put('C', 3);
+        letterMap.put('D', 2);
+        letterMap.put('E', 1);
+        letterMap.put('F', 4);
+        letterMap.put('G', 2);
+        letterMap.put('H', 4);
+        letterMap.put('I', 1);
+        letterMap.put('J', 8);
+        letterMap.put('K', 5);
+        letterMap.put('L', 1);
+        letterMap.put('M', 3);
+        letterMap.put('N', 1);
+        letterMap.put('O', 1);
+        letterMap.put('P', 3);
+        letterMap.put('Q', 10);
+        letterMap.put('R', 1);
+        letterMap.put('S', 1);
+        letterMap.put('T', 1);
+        letterMap.put('U', 1);
+        letterMap.put('V', 4);
+        letterMap.put('W', 4);
+        letterMap.put('X', 8);
+        letterMap.put('Y', 4);
+        letterMap.put('Z', 10);
+        letterMap.put('?', 3);
+    }
+}
